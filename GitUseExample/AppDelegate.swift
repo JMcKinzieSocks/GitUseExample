9d2207a7cddf4cb5070e384293c16febe07d2dd8
Jackson McKinzie <jacksondmckinzie@gmail.com>
2020-10-02 13:17:25 -0500

    Added UI button commented out a thing

--- GitUseExample/AppDelegate.swift
+++ GitUseExample/AppDelegate.swift
@@ -16,7 +16,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication,
                      didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
-        print("Application did finish launching")
+        //print("Application did finish launching")
         return true
     }
 

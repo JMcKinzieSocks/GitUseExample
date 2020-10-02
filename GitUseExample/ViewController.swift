e446af90843b542e9d56acb11b20e5814ef220c2
Jackson McKinzie <jacksondmckinzie@gmail.com>
2020-10-02 13:07:39 -0500

    Initial Commit

---
+++ GitUseExample/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  GitUseExample
+//
+//  Created by McKinzie, Jackson D on 10/2/20.
+//  Copyright © 2020 McKinzie, Jackson D. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
